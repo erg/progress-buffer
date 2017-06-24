c93bc0d02f549d2835a1194092d3e1cf7b53ba83
Doug Coleman <doug.coleman@gmail.com>
2017-06-23 20:19:39 -0500

    index: Fix up the code a bit more.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const sleep = require('sleep');
 
 // https://stackoverflow.com/questions/24531751/how-can-i-split-a-string-containing-emoji-into-an-array
 var emojiStringToArray = function (str) {
-  split = str.split(/([\uD800-\uDBFF][\uDC00-\uDFFF])/);
-  arr = [];
+  let split = str.split(/([\uD800-\uDBFF][\uDC00-\uDFFF])/);
+  var arr = [];
   for (var i = 0; i < split.length; i++) {
-    char = split[i]
+    let char = split[i]
     if (char !== "") {
       arr.push(char);
     }
@@ -36,19 +36,13 @@ function printBuffer(buffer) {
 }
 
 function eraseBuffer(linesWritten) {
-  var erase = '';
-  if(linesWritten == 0) {
-    erase = '\33[2K\r'; // necessary
-  } else {
-    erase = '\33[2K\033[F'.repeat(linesWritten);
-  }
+  let erase = (linesWritten == 0) ? erase = '\33[2K\r': '\33[2K\033[F'.repeat(linesWritten);
   process.stdout.write(erase);
 }
 
 // [a,b)
 function randomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  var min = Math.ceil(min), max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
@@ -56,16 +50,16 @@ function main() {
   var buffer = {};
   
   for (var i = 0; i < process.stdout.rows - 2; i++) {
-    let col = randomInt(0, process.stdout.columns - 2);
-    let state = randomInt(0, theme.length);
+    let col = randomInt(0, process.stdout.columns - 2),
+      state = randomInt(0, theme.length);
     buffer[i] = [0, state]
   }
 
   do {
-    var linesWritten = printBuffer(buffer);
+    let linesWritten = printBuffer(buffer);
     sleep.usleep(50000);
     eraseBuffer(linesWritten);
-    var len = Object.keys(buffer).length;
+    let len = Object.keys(buffer).length;
     for (var key in buffer) {
       let [col, state] = buffer[key];
       if(col == process.stdout.columns - 2) {
